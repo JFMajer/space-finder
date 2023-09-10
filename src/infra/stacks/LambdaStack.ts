14dc00e0b974b9d2a90b6eaa06e0f13fb6b51720
JFMajer <jakubmajer@live.com>
2023-09-10 14:07:08 +0200

    better response json

--- src/infra/stacks/LambdaStack.ts
+++ src/infra/stacks/LambdaStack.ts
@@ -5,40 +5,46 @@ import { join } from "path";
 import { LambdaIntegration } from "aws-cdk-lib/aws-apigateway";
 import { ITable } from "aws-cdk-lib/aws-dynamodb";
 import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
+import { PolicyStatement, Effect } from "aws-cdk-lib/aws-iam";
 
 interface LamdaStackProps extends cdk.StackProps {
-    spacesTable: ITable;
+  spacesTable: ITable;
 }
 
-export class LambdaStack extends cdk.Stack{
+export class LambdaStack extends cdk.Stack {
+  public readonly lambdaIntegration: LambdaIntegration[] = [];
 
-    public readonly lambdaIntegration: LambdaIntegration[] = [];
+  constructor(scope: Construct, id: string, props: LamdaStackProps) {
+    super(scope, id, props);
 
-    constructor(scope: Construct, id: string, props: LamdaStackProps) {
-        super(scope, id, props);
+    //my hello function
+    const lambda = new NodejsFunction(this, "HelloLambda", {
+      runtime: Runtime.NODEJS_18_X,
+      handler: "handler",
+      entry: join(__dirname, "..", "..", "services", "hello.ts"),
+      environment: {
+        TABLE_NAME: props.spacesTable.tableName,
+      },
+      logRetention: 1,
+    });
 
-        //my hello function
-        const lambda = new NodejsFunction(this, 'HelloLambda', {
-            runtime: Runtime.NODEJS_18_X,
-            handler: "handler",
-            entry: join(__dirname, '..', '..', 'services', 'hello.ts'),
-            environment: {
-                TABLE_NAME: props.spacesTable.tableName,
-            },
-            logRetention: 1,
-        })
+    //function that lists s3 bucket
+    const s3lambda = new NodejsFunction(this, "S3Lambda", {
+      runtime: Runtime.NODEJS_18_X,
+      handler: "handler",
+      entry: join(__dirname, "..", "..", "services", "s3lambda.ts"),
+      logRetention: 1,
+    });
 
-        //function that lists s3 bucket
-        const s3lambda = new NodejsFunction(this, 'S3Lambda', {
-            runtime: Runtime.NODEJS_18_X,
-            handler: "handler",
-            entry: join(__dirname, '..', '..', 'services', 's3lambda.ts'),
-            logRetention: 1,
-        })
+    s3lambda.addToRolePolicy(
+      new PolicyStatement({
+        effect: Effect.ALLOW,
+        actions: ["s3:ListAllMyBuckets", "s3:ListBucket"],
+        resources: ["arn:aws:s3:::*"],
+      })
+    );
 
-        this.lambdaIntegration.push(new LambdaIntegration(lambda));
-        this.lambdaIntegration.push(new LambdaIntegration(s3lambda));
-
-    }
-
+    this.lambdaIntegration.push(new LambdaIntegration(lambda));
+    this.lambdaIntegration.push(new LambdaIntegration(s3lambda));
+  }
 }
--- src/services/s3lambda.ts
+++ src/services/s3lambda.ts
@@ -1,12 +1,25 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResultV2, Context } from "aws-lambda";
+import { S3Client, ListBucketsCommand } from "@aws-sdk/client-s3";
+
+
+const s3Client = new S3Client({ region: "eu-north-1" });
 
 async function handler(event: APIGatewayProxyEvent, context: Context): Promise<APIGatewayProxyResultV2> {
+    
+    const command = new ListBucketsCommand({});
+    const lsbucketsResponse = await s3Client.send(command);
+    const bucketNames = lsbucketsResponse.Buckets?.map((bucket) => bucket.Name);
+
     const response: APIGatewayProxyResultV2 = {
         statusCode: 200,
-        body: JSON.stringify('Hello from s3 Lambda!'),
+        body: JSON.stringify({
+            message: 'Hello from lambda, here are your bucket names:',
+            buckets: bucketNames,
+        })
     }
-    console.log('event', event);
+
     return response;
+   
 }
 
 export { handler };
