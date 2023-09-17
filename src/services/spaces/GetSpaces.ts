058f26c2f85faef2e10c1270b720c774d788e8ac
JFMajer <jakubmajer@live.com>
2023-09-17 17:52:28 +0200

    getitem lambda and query parameters

--- src/services/spaces/GetSpaces.ts
+++ src/services/spaces/GetSpaces.ts
@@ -1,31 +1,88 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResultV2 } from "aws-lambda";
-import { DynamoDBClient, ScanCommand } from "@aws-sdk/client-dynamodb";
+import {
+  DynamoDBClient,
+  ScanCommand,
+  GetItemCommand,
+} from "@aws-sdk/client-dynamodb";
 
 async function getSpaces(
   event: APIGatewayProxyEvent,
   ddbclient: DynamoDBClient
 ): Promise<APIGatewayProxyResultV2> {
-  const result = await ddbclient.send(
-    new ScanCommand({
-      ReturnConsumedCapacity: "TOTAL",
-      TableName: process.env.TABLE_NAME,
-    })
-  );
+  const queryParams = event.queryStringParameters;
 
-  console.log("result", result);
+  if (!queryParams) {
+    try {
+      const scanResult = await ddbclient.send(
+        new ScanCommand({
+          ReturnConsumedCapacity: "TOTAL",
+          TableName: process.env.TABLE_NAME,
+        })
+      );
 
-  const consumedCapacity = result.ConsumedCapacity;
-  console.log("consumedCapacity", consumedCapacity);
+      const result = scanResult.Items;
+      console.log("result", result);
+      const consumedCapacity = scanResult.ConsumedCapacity;
+      console.log("consumedCapacity", consumedCapacity);
 
-  const response: APIGatewayProxyResultV2 = {
-    statusCode: 201,
-    body: JSON.stringify({
-      result: result.Items,
-      consumedCapacity: consumedCapacity,
-    }),
-  };
+      return {
+        statusCode: 200,
+        body: JSON.stringify({
+          result: result,
+          consumedCapacity: consumedCapacity,
+        }),
+      };
+    } catch (error: any) {
+      console.log("error", error);
+      return {
+        statusCode: 500,
+        body: JSON.stringify(error.message),
+      };
+    }
+  } else if (queryParams && queryParams.id) {
+    const id = queryParams.id;
+    try {
+      const getItemResult = await ddbclient.send(
+        new GetItemCommand({
+          TableName: process.env.TABLE_NAME,
+          Key: {
+            id: { S: id },
+          },
+          ReturnConsumedCapacity: "TOTAL",
+        })
+      );
+      const consumedCapacity = getItemResult.ConsumedCapacity;
 
-  return response;
+      if(!getItemResult.Item) {
+        return {
+          statusCode: 404,
+          body: JSON.stringify({
+            message: "Not Found",
+            consumedCapacity: consumedCapacity,
+          }),
+        }
+      } else {
+        return {
+          statusCode: 200,
+          body: JSON.stringify({
+            result: getItemResult.Item,
+            consumedCapacity: consumedCapacity,
+          }),
+        };
+      }
+    } catch (error: any) {
+      console.log("error", error);
+      return {
+        statusCode: 500,
+        body: JSON.stringify(error.message),
+      };
+    }
+  }
+  return {
+    statusCode: 400,
+    body: JSON.stringify("Bad Request"),
+  }
+
 }
 
 export { getSpaces };
